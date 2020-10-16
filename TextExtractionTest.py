97d9550b7a83cb043578088fb7113c668bbc271c
ManuMazidi <manuel.mazidi@gmail.com>
2020-10-16 15:14:57 +0200

    Added LSA Model with Plot

--- TextExtractionTest.py
+++ TextExtractionTest.py
@@ -27,7 +27,6 @@ for filename in file_paths:
     find_date = re.search("[0-9]{8}", filename)
     pdf_date = datetime.strptime(find_date.group(), '%Y%m%d')
     pdf_datetime = pdf_date.date()
-
     communication_dates.append(pdf_datetime)
 
     # read PDF
@@ -47,6 +46,7 @@ for filename in file_paths:
     # clean up extracted text, you may want to add or remove some features depending on PDF
     # this works for FED FOMC Minutes
     new_content = page_content.replace('\n', '')
+    new_content = new_content.replace(' o ', '')
     new_content = new_content.replace(' ', ' ')
     new_content = new_content.replace('  ', ' ')
     new_content = new_content.replace('Š', ' ')
@@ -62,6 +62,9 @@ for filename in file_paths:
 
 # PREPROCESSING OF RAW TEXT
 # I use spaCy for this, load their english (en) model
+# note that the following line works in jupyter notebook but not directly in PyCharm
+# work-around to load in PyCharm: Go to Preferences/Python Interpreter, copy path to python interpreter
+# in Terminal run: FULL_PATH_TO_PYTHON_INTERPRETER -m spacy download en
 nlp = en_core_web_sm.load()
 
 
@@ -121,7 +124,7 @@ for clean_tokens in preprocessed_texts:
 
     # 5 commonly occurring words with their frequencies
     common_words = word_freq.most_common(5)
-    print(common_words)
+    #print(common_words)
 
 # Unique words that only appear once per PDF
 # unique_words = [word for (word, freq) in word_freq.items() if freq == 1]
@@ -131,3 +134,93 @@ for clean_tokens in preprocessed_texts:
 #for text in clean_docs:
 #    for ent in text.ents:
 #        print(ent.text, ent.start_char, ent.end_char, ent.label_, spacy.explain(ent.label_))
+
+
+
+# PREPARE CORPUS FROM 'preprocessed_texts'
+import os.path
+from gensim import corpora
+from gensim.corpora import Dictionary
+from gensim.models import LsiModel
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from gensim.models.coherencemodel import CoherenceModel
+import matplotlib.pyplot as plt
+
+def prepare_corpus(doc_clean):
+    """
+    Input  : clean document
+    Purpose: create term dictionary of our courpus and Converting list of documents (corpus) into Document Term Matrix
+    Output : term dictionary and Document Term Matrix
+    """
+    # Creating the term dictionary of our courpus, where every unique term is assigned an index. dictionary = corpora.Dictionary(doc_clean)
+    dictionary = corpora.Dictionary(doc_clean)
+    # Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
+    doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
+    # generate LDA model
+    return dictionary,doc_term_matrix
+
+prepare_corpus(preprocessed_texts)
+
+# create a dictionary object, e.g. if you want to know of how many unique tokens your dictionary from your corpus consists of:
+dct = Dictionary(preprocessed_texts)
+
+
+
+# CREATE GENSIM LSA MODEL
+def create_gensim_lsa_model(doc_clean,number_of_topics,words):
+    """
+    Input  : clean document, number of topics and number of words associated with each topic
+    Purpose: create LSA model using gensim
+    Output : return LSA model
+    """
+    dictionary,doc_term_matrix=prepare_corpus(doc_clean)
+    # generate LSA model
+    lsamodel = LsiModel(doc_term_matrix, num_topics=number_of_topics, id2word = dictionary)  # train model
+    print(lsamodel.print_topics(num_topics=number_of_topics, num_words=words))
+    return lsamodel
+
+
+def compute_coherence_values(dictionary, doc_term_matrix, doc_clean, stop, start=2, step=3):
+    """
+    Input   : dictionary : Gensim dictionary
+              corpus : Gensim corpus
+              texts : List of input texts
+              stop : Max num of topics
+    purpose : Compute c_v coherence for various number of topics, so that optimal number of topics can be evaluated
+    Output  : model_list : List of LSA topic models
+              coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    coherence_values = []
+    model_list = []
+    for num_topics in range(start, stop, step):
+        # generate LSA model
+        model = LsiModel(doc_term_matrix, num_topics=number_of_topics, id2word = dictionary)  # train model
+        model_list.append(model)
+        coherencemodel = CoherenceModel(model=model, texts=doc_clean, dictionary=dictionary, coherence='c_v')
+        coherence_values.append(coherencemodel.get_coherence())
+    return model_list, coherence_values
+
+def plot_graph(doc_clean,start, stop, step):
+    dictionary,doc_term_matrix=prepare_corpus(doc_clean)
+    model_list, coherence_values = compute_coherence_values(dictionary, doc_term_matrix,doc_clean,
+                                                            stop, start, step)
+    # Show graph
+    x = range(start, stop, step)
+    plt.plot(x, coherence_values)
+    plt.xlabel("Number of Topics")
+    plt.ylabel("Coherence score")
+    plt.legend(("coherence_values"), loc='best')
+    plt.show()
+
+
+# LSA Model
+number_of_topics=8
+words=10
+#document_list,titles=load_data("","articles.txt")
+model=create_gensim_lsa_model(preprocessed_texts,number_of_topics,words)
+
+# Create Plot from LSA Model
+start,stop,step=2,12,1
+plot_graph(preprocessed_texts,start,stop,step)
